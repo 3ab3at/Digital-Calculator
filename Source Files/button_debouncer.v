2a0800668986b2a61d28f66423ef51c35edc2144
Ibrahim Abdelmaksoud Gohar <49319772+abdelmaksou@users.noreply.github.com>
2022-05-18 11:03:45 +0200

    Update button_debouncer.v

--- Source Files/button_debouncer.v
+++ Source Files/button_debouncer.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.05.2022 14:28:52
-// Design Name: 
-// Module Name: btn_deb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module btn_deb(input w, clk, rst, output z);
     reg D1,D2,D3;
